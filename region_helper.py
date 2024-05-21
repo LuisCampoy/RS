3b11f0b1b5e48346ca91c65d99b985c285951501
LuisCampoy <105428538+LuisCampoy@users.noreply.github.com>
2024-05-21 07:49:59 -0400

    Add files via upload

--- region_helper.py
+++ region_helper.py
@@ -4,44 +4,47 @@
 
 import pandas as pd
 
+
 # calculate maximum absolute value within the ROI in X, Y and Z axis
 def get_region(df, regions_of_interest, window_size, step_size):
-     
-    for k in range(len(regions_of_interest)):    
-        print('ROI ',
+
+    for k in range(len(regions_of_interest)):
+        print(
+            "ROI ",
             k + 1,
-            ': (',
-            df['TimeStamp'][
-                regions_of_interest[k][0] * step_size
-            ],
-            ',',
-            df['TimeStamp'][
-                regions_of_interest[k][0] * step_size + window_size
-            ],
-            ')',
+            ": (",
+            df["TimeStamp"][regions_of_interest[k][0] * step_size],
+            ",",
+            df["TimeStamp"][regions_of_interest[k][0] * step_size + window_size],
+            ")",
         )
 
+
 def get_max_abs_value(df, regions_of_interest, window_size, step_size):
-    ''' Create a new pd.DataFrafe with the 3 columns (X, Y, Z) and region of interest
+    """Create a new pd.DataFrafe with the 3 columns (AccX, AccY, AccZ) and region of interest
 
     Args:
 
     Returns:
 
-    '''
+    """
+
+    raise NotImplementedError("get_max_abs_value is missing code")
+
     # Assuming 'df' is your DataFrame and x, y are the row indices
     # Replace 1 and 4 with the column indices if needed (Python uses 0-based indexing)
-   
-    for i in range(len(regions_of_interest)):  
-        
-        selected_data = df.loc[[regions_of_interest[i][0] * step_size
-            ] : [regions_of_interest[i][0] * step_size + window_size], 1:4]
 
-        print(f'selected_data are... {selected_data}')
+    for i in range(len(regions_of_interest)):
 
-    max_abs_value= selected_data.abs().max().max()
+        selected_data = df.loc[
+            [regions_of_interest[i][0] * step_size] : [
+                regions_of_interest[i][0] * step_size + window_size
+            ],
+            1:4,
+        ]
+
+        print(f"selected_data are... {selected_data}")
+
+        max_abs_value = selected_data.abs().max().max()
 
     return max_abs_value
-
-
-
