f12aa2b3ed28ff98d1028d077018797e318f63aa
LuisCampoy <105428538+LuisCampoy@users.noreply.github.com>
2024-05-21 14:57:39 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -16,16 +16,16 @@ def main() -> None:
     window_size: int = 10000
     step_size: int = 2000
 
-    #file_path: str = input("Enter file name: ")
-    file_path: str = '344717_RED.csv'
-    
+    # file_path: str = input("Enter file name: ")
+    file_path: str = "344717_RED.csv"
+
     df: pd.DataFrame = read_csv_file(file_path)
-   
+
     if df is not None:
-    
-        print('csv file returned to main() successfully')
-  
-    AccZ_sd: list[float] = calculate_window_sd(df['AccZ'][1:], window_size, step_size)
+
+        print("csv file returned to main() successfully")
+
+    AccZ_sd: list[float] = calculate_window_sd(df["AccZ"][1:], window_size, step_size)
     """ Extracts a list of float values from df, 
         a dictionary, where "AccZ" is a key. [1:] starting from the second element
 
@@ -40,26 +40,26 @@ def main() -> None:
 
     print(regions_of_interest)
 
-    print('Regions of Interest detected successfully...')
-    
+    print("Regions of Interest detected successfully...")
+
     number_of_failed_attempts = get_attempts(regions_of_interest)
 
-    print(f'Number of Failed Attempts = {number_of_failed_attempts}')
+    print(f"Number of Failed Attempts = {number_of_failed_attempts}")
 
     get_region(df, regions_of_interest, window_size, step_size)
-    
-    print('ROI rows printed successfully...')
 
-    #get_plot(df, regions_of_interest, window_size, step_size, file_path)  
+    print("ROI rows printed successfully...")
 
-    get_amax_sa(df, regions_of_interest, window_size, step_size)
-    
-   # print(amax_sa)
-    
-    #print('amax_sa printed sucessfully...')
-    
-    
+    # get_plot(df, regions_of_interest, window_size, step_size, file_path)
+
+    amax_sa: list = list(get_amax_sa(df, regions_of_interest, window_size, step_size))
+
+    # print(amax_sa)
+
+    # print('amax_sa printed sucessfully...')
+
     get_amax_ua()
+
 
 if __name__ == "__main__":
     main()
--- region_helper.py
+++ region_helper.py
@@ -21,46 +21,40 @@ def get_region(df, regions_of_interest, window_size, step_size):
 
 
 def get_amax_sa(df, regions_of_interest, window_size, step_size) -> list[float]:
-    """Create a new pd.DataFrafe with the 2 columns (AccX, AccY) within the last region of interest
-        Calculate abs value of each colum and then the max value of each column
+    """Create a new pd.DataFrafe with the 2 columns (AccX, AccY) within the last region of interest (successful attempt)
+        Calculate abs value of each colum and then return the max value from each column
 
     Args:
         df:
         regions_of_interest:
         window_size:
-        step_size
+        step_size:
 
     Returns:
         list[float]
 
     """
 
-    # raise NotImplementedError('Sorry, get_amax_sa() is not implemented yet...')
-
-    # Assuming 'df' is your DataFrame and x, y are the row indices
-    # Replace 1 and 4 with the column indices if needed (Python uses 0-based indexing)
-    
+    raise NotImplementedError("Sorry, get_amax_sa() is not implemented yet...")
 
     j = len(regions_of_interest)
     window = []
     for region in regions_of_interest:
         start_index = region[0] * step_size
         end_index = start_index + window_size
-        
-        AccX_val = df['AccX'][start_index]
-        AccY_val = df['AccY'][end_index]
-        
+
+        AccX_val = df["AccX"][start_index]
+        AccY_val = df["AccY"][end_index]
+
         window.append((AccX_val, AccY_val))
-        
-    selected_data = pd.DataFrame(window, columns =['AccX', 'AccY'])
-        
-        
-    
+
+    selected_data = pd.DataFrame(window, columns=["AccX", "AccY"])
+
     # selected_data = df.loc[[regions_of_interest[j][0] * step_size] : [regions_of_interest[j][0] * step_size + window_size]]
-    #selected_data = df.iloc[regions_of_interest[0][0] : regions_of_interest[0][0] + window_size,"AccX":"AccY"]
+    # selected_data = df.iloc[regions_of_interest[0][0] : regions_of_interest[0][0] + window_size,"AccX":"AccY"]
 
     print(selected_data)
-    print('Selected_Data printed...')
+    print("Selected_Data printed...")
 
     # amax_sa = selected_data.abs().max().max()
 
